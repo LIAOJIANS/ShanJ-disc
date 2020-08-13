240c823b0db898af54782d0a4cac77c8950b2715
LIAOJIANS <272781702@qq.com>
2020-08-13 22:53:56 +0800

    Version updata

---
+++ src/pages/CompleteTransfer/CompleteTransfer.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <download-list :list="list" />
+  </div>
+</template>
+
+<script>
+import { uploadList } from '@/api/file'
+import DownloadList from '@/components/DownloadList/DownloadList'
+export default {
+  name: "CompleteTransfer",
+
+  components: {
+    DownloadList
+  },
+
+  data() {
+    return {
+     list: []
+    }
+  },
+  mounted() {
+    this.loadData()
+  },
+
+  methods: {
+    loadData() {
+      uploadList(this.$store.getters.userInfo.u_id).then(res => {
+        this.list = res.data
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
